0f9ceb38fece7c6ecaa3e5212b0db2de044133ce
Ray Chow <raychow.info@gmail.com>
2014-10-18 02:20:46 +0800

    Added buttons in overwrite dialog.

--- EightZip/OverwriteDialog.cpp
+++ EightZip/OverwriteDialog.cpp
@@ -61,19 +61,23 @@ void OverwriteDialog::__Create(TString tstrPath,
     auto *pSizerFileInfo = new wxStaticBoxSizer(
         new wxStaticBox(this, wxID_ANY, wxEmptyString), wxVERTICAL);
 
+    pSizerFileInfo->AddSpacer(6);
     pSizerFileInfo->Add(new wxStaticText(this, wxID_ANY,
-        _("The following file already exists:")));
+        _("The following file already exists:")),
+        wxSizerFlags().Border(wxBOTTOM, 3));
     auto pTextCtrlPath = new wxTextCtrl(this, wxID_ANY, tstrPath,
-        wxDefaultPosition, wxSize(-1, 70), wxTE_MULTILINE);
+        wxDefaultPosition, wxSize(350, 60), wxTE_MULTILINE);
     pTextCtrlPath->SetEditable(false);
-    pSizerFileInfo->Add(pTextCtrlPath, wxSizerFlags().Expand());
+    pSizerFileInfo->Add(pTextCtrlPath,
+        wxSizerFlags().Border(wxBOTTOM, 6).Expand());
 
     pSizerFileInfo->Add(new wxStaticText(this, wxID_ANY,
         _("Would you like to replace the existing file")));
 
     auto *pSizerExistFile = new wxBoxSizer(wxHORIZONTAL);
     pSizerExistFile->Add(new wxStaticBitmap(this, wxID_ANY,
-        m_imageList.GetIcon(FileInfo::GetIconIndex(tstrPath))));
+        m_imageList.GetIcon(FileInfo::GetIconIndex(tstrPath))),
+        wxSizerFlags().Border(wxRIGHT, 6));
     auto *pSizerExistFileInfo = new wxBoxSizer(wxVERTICAL);
     if (oun64ExistSize)
     {
@@ -90,14 +94,16 @@ void OverwriteDialog::__Create(TString tstrPath,
             FormatDateTime(*oftExistModified))));
     }
     pSizerExistFile->Add(pSizerExistFileInfo);
-    pSizerFileInfo->Add(pSizerExistFile);
+    pSizerFileInfo->Add(pSizerExistFile,
+        wxSizerFlags().Border(wxTOP | wxBOTTOM, 6));
 
     pSizerFileInfo->Add(new wxStaticText(this, wxID_ANY,
         _("with this one?")));
 
     auto *pSizerNewFile = new wxBoxSizer(wxHORIZONTAL);
     pSizerNewFile->Add(new wxStaticBitmap(this, wxID_ANY,
-        m_imageList.GetIcon(FileInfo::GetIconIndex(tstrPath))));
+        m_imageList.GetIcon(FileInfo::GetIconIndex(tstrPath))),
+        wxSizerFlags().Border(wxRIGHT, 6));
     auto *pSizerNewFileInfo = new wxBoxSizer(wxVERTICAL);
     if (oun64NewSize)
     {
@@ -112,10 +118,32 @@ void OverwriteDialog::__Create(TString tstrPath,
             FormatDateTime(*oftNewModified))));
     }
     pSizerNewFile->Add(pSizerNewFileInfo);
-    pSizerFileInfo->Add(pSizerNewFile);
+    pSizerFileInfo->Add(pSizerNewFile,
+        wxSizerFlags().Border(wxTOP | wxBOTTOM, 6));
 
     pSizerMain->Add(pSizerFileInfo,
-        wxSizerFlags().Expand().Border(wxLEFT | wxRIGHT, 8));
+        wxSizerFlags().Expand().Border(wxLEFT | wxRIGHT | wxBOTTOM, 8));
 
-    SetSizer(pSizerMain);
+    auto *pSizerButton = new wxGridSizer(2, 4, 3, 3);
+
+    auto pButtonYes = new wxButton(this, wxID_ANY, _("&Yes"));
+    pSizerButton->Add(pButtonYes);
+    auto pButtonNo = new wxButton(this, wxID_ANY, _("&No"));
+    pSizerButton->Add(pButtonNo);
+    auto pButtonRename = new wxButton(this, wxID_ANY, _("&Rename"));
+    pSizerButton->Add(pButtonRename);
+    pSizerButton->Add(new wxWindow());
+    auto pButtonYesToAll = new wxButton(this, wxID_ANY, _("Yes to &All"));
+    pSizerButton->Add(pButtonYesToAll);
+    auto pButtonNoToAll = new wxButton(this, wxID_ANY, _("No to A&ll"));
+    pSizerButton->Add(pButtonNoToAll);
+    auto pButtonRenameAll = new wxButton(this, wxID_ANY, _("R&ename All"));
+    pSizerButton->Add(pButtonRenameAll);
+    auto pButtonCancel = new wxButton(this, wxID_ANY, _("&Cancel"));
+    pSizerButton->Add(pButtonCancel);
+
+    pSizerMain->Add(pSizerButton,
+        wxSizerFlags().Border(wxBOTTOM, 8).Center());
+
+    SetSizerAndFit(pSizerMain);
 }
