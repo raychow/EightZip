6de98b2cf14f427020993e7cb2252ed7cc48834d
Ray Chow <raychow.info@gmail.com>
2014-11-28 00:59:30 +0800

    Fixed a bug in ArchiveProperty::GetPassword when calling in non-main thread.

--- EightZip/ArchiveProperty.cpp
+++ EightZip/ArchiveProperty.cpp
@@ -11,11 +11,27 @@ boost::optional<TString> ArchiveProperty::GetPassword() const
 {
     if (!m_otstrPassword)
     {
-        PasswordDialog passwordDialog {
-            wxTheApp->GetTopWindow(), wxID_ANY, _("Enter password") };
-        if (passwordDialog.ShowModal() == wxID_OK)
+        if (wxThread::IsMain())
         {
-            m_otstrPassword = passwordDialog.GetValue();
+            PasswordDialog passwordDialog {
+                wxTheApp->GetTopWindow(), wxID_ANY, _("Enter password") };
+            if (passwordDialog.ShowModal() == wxID_OK)
+            {
+                m_otstrPassword = passwordDialog.GetValue();
+            }
+        }
+        else
+        {
+            promise<boost::optional<TString>> result;
+            wxTheApp->CallAfter([&]() {
+                PasswordDialog passwordDialog {
+                    wxTheApp->GetTopWindow(), wxID_ANY, _("Enter password") };
+                if (passwordDialog.ShowModal() == wxID_OK)
+                {
+                    result.set_value(passwordDialog.GetValue());
+                }
+            });
+            m_otstrPassword = result.get_future().get();
         }
     }
     return m_otstrPassword;
